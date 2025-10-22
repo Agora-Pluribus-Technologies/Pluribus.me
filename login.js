eb3261cc4b86895afe357ef5dd8a4a7b8c232767
AgoraPluribusMichael <michael@agorapluribus.com>
2025-10-22 16:12:59 -0400

    Fix const

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 // Replace with your actual Application ID and callback URL
-const CLIENT_ID =
-  "12328ed7f6e7e0ffae8d10d8531df71aeffd7db927c966ffc763bf07e8800656";
+const NETLIFY_CLIENT_ID =
+  "YrSXJx6H250qmnq5dgb4rlRdynecY16jGKhNcJJx60E";
 const REDIRECT_URI = "https://pluribus-me.pages.dev/oauth/callback";
 // const SCOPE = "api read_repository write_repository";
 const AUTH_URL = "https://app.netlify.com/oauth/authorize";
@@ -8,7 +8,7 @@ const AUTH_URL = "https://app.netlify.com/oauth/authorize";
 document.getElementById("login-button").addEventListener("click", () => {
   // Build the authorization URL
   const params = new URLSearchParams({
-    client_id: CLIENT_ID,
+    client_id: NETLIFY_CLIENT_ID,
     redirect_uri: REDIRECT_URI,
     response_type: "code",
     // scope: SCOPE,
