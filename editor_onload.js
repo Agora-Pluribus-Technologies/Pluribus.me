ae379bb31c8a21d2a2a5d25bef16711660100320
AgoraPluribusMichael <michael@agorapluribus.com>
2025-10-24 02:38:52 -0400

    Site creation WIP 2

--- editor_onload.js
+++ editor_onload.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   // Save token from callback
-  saveToken();
+  const accessToken = saveToken();
 
   // Get account type "free"
   const url = "https://api.netlify.com/api/v1/accounts/types";
   const payload = {
     method: "GET",
     headers: {
-      "Authorization": `Bearer ${accessToken}`,
-    }
-  }
+      Authorization: `Bearer ${accessToken}`,
+    },
+  };
   const response = apiRequest(url, payload);
   const data = response.json();
-  for (let i=0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     var typeObj = data[i];
     if (typeObj.name.toLowerCase() == "free") {
       console.log("Free account type_id: " + typeObj.id);
@@ -23,25 +23,26 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function saveToken() {
-    // 1. Parse token from URL fragment (after the '#')
-    const hashParams = new URLSearchParams(window.location.hash.slice(1));
-    const token = hashParams.get("access_token");
+  // 1. Parse token from URL fragment (after the '#')
+  const hashParams = new URLSearchParams(window.location.hash.slice(1));
+  const token = hashParams.get("access_token");
 
-    // 2. If a token is present, store it in sessionStorage
-    if (token) {
-      sessionStorage.setItem("token", token);
+  // 2. If a token is present, store it in sessionStorage
+  if (token) {
+    sessionStorage.setItem("token", token);
 
-      // 3. Clean the URL bar by removing the fragment
-      window.history.replaceState({}, document.title, window.location.pathname);
-    }
+    // 3. Clean the URL bar by removing the fragment
+    window.history.replaceState({}, document.title, window.location.pathname);
+  }
+  return token;
 }
 
 async function apiRequest(url, body) {
   const accessToken = sessionStorage.getItem("token");
   if (accessToken) {
-      window.alert("✅ Token available: " + accessToken);
+    window.alert("✅ Token available: " + accessToken);
   } else {
-      console.warn("⚠️ No token found. Redirect user to log in.");
+    console.warn("⚠️ No token found. Redirect user to log in.");
   }
   const response = await fetch(url, body);
 
@@ -50,7 +51,6 @@ async function apiRequest(url, body) {
   }
 
   const data = await response.json();
-  console.log(data);           // prints the parsed JSON
-  return data;                 // use it in your code
+  console.log(data);
+  return data;
 }
-
