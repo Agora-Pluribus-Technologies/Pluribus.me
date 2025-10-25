e43606c0ae745e279735d83e7f7af897f9528d1c
AgoraPluribusMichael <michael@agorapluribus.com>
2025-10-25 03:30:24 -0400

    Fix worker format

--- functions/netlify/create-site.js
+++ functions/netlify/create-site.js
@@ -1,21 +1,22 @@
-export default {
-  async fetch(request, env) {
-    const authorization = request.headers.get("Authorization");
-    const zipFile = await request.arrayBuffer();
-    const netlifySitesUrl = `https://api.netlify.com/api/v1/sites`;
-    const resp = await fetch(netlifySitesUrl, {
-      method: "POST",
-      headers: {
-        Authorization: authorization,
-        "Content-Type": "application/zip",
-      },
-      body: zipFile,
+export async function onRequestPost(context) {
+  const request = context.request;
+  const authorization = request.headers.get("Authorization");
+  const zipFile = await request.arrayBuffer();
+  const netlifySitesUrl = `https://api.netlify.com/api/v1/sites`;
+  const resp = await fetch(netlifySitesUrl, {
+    method: "POST",
+    headers: {
+      Authorization: authorization,
+      "Content-Type": "application/zip",
+    },
+    body: zipFile,
+  });
+
+  if (!resp.ok) {
+    return new Response("Could not create Netlify site", {
+      status: resp.status,
     });
+  }
 
-    if (!resp.ok) {
-      return new Response("Could not create Netlify site", { status: resp.status });
-    }
-
-    return resp;
-  },
-};
+  return resp;
+}
