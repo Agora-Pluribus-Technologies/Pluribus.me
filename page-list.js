3aab859ecd907a9c40eb097299db2bff9a567895
AgoraPluribusMichael <michael@agorapluribus.com>
2025-11-01 20:15:21 -0400

    Load markdown from site

--- page-list.js
+++ page-list.js
@@ -2,12 +2,20 @@ document.addEventListener("DOMContentLoaded", async function () {
   const sites = await getSiteListNetlify();
   const sitesListPanel = document.getElementById("sitesListPanel");
   for (const site of sites) {
-    const siteDiv = document.createElement("div");
-    siteDiv.style.border = "1px solid #ccc";
-    siteDiv.style.padding = "10px";
-    siteDiv.style.marginBottom = "10px";
-    siteDiv.innerHTML = site.name;
-    sitesListPanel.appendChild(siteDiv);
+    const siteButton = document.createElement("button");
+    siteButton.style.border = "1px solid #ccc";
+    siteButton.style.padding = "10px";
+    siteButton.style.marginBottom = "10px";
+    siteButton.style.maxWidth = "100px";
+    siteButton.innerHTML = site.name;
+    siteButton.addEventListener("click", () => {
+      console.log(`Selected site: ${site.name} (ID: ${site.id})`);
+      const markdown = fetchMarkdownFromSite(site.name);
+      markdown.then((data) => {
+        editor.setMarkdown(data);
+      });
+    });
+    sitesListPanel.appendChild(siteButton);
   }
 });
 
@@ -17,6 +25,19 @@ async function getSiteListNetlify() {
   if (!oauthToken) return false;
 
   const response = await fetch("https://api.netlify.com/api/v1/sites", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${oauthToken}`,
+    },
+  });
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function fetchMarkdownFromSite(siteName) {
+  const response = await fetch(`${siteName}.netlify.app/index.md`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${oauthToken}`,
